b194af50f214c4d76352bbb178b5b8346359bd5c
Cenildo <50890741+Cenildo@users.noreply.github.com>
2019-05-23 19:24:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,41 @@
+<html>
+<head> 
+<meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="css/stilo.css">
+<!-- comentario -->
+
+<title>Nego Ney</title>
+</head>
+<body>
+<div class="container">
+<h1>Nego Ney</h1>
+<form method="post" class="Form-inline">
+
+<div class="row">
+
+<div class="col">
+<input type="text" placeholder="bidoginfinin" name="nomecomputador" class="form-control">
+</div>
+
+<div class="col">
+<input type="text" placeholder="cabelinaregua" name="ip" class="form-control">
+</div>
+
+<div class="col">
+<input type="submit" class="btn btn-outline-secondary" value="salvar"/>
+</div>
+</form>
+
+<div class="row">
+<div class="col">
+
+<div class="bloco online"></div>
+<div class="bloco offline"></div>
+
+</div>
+</div>
+
+</div>
+</body>
+</html>
